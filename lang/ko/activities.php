4968658ce2d5db568be7275db4caf5dc8656b0f8
Gabriel Amoedo <gbas95@gmail.com>
2024-01-22 21:43:44 -0400

    first commit at sescwiki

---
+++ lang/ko/activities.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Activity text strings.
+ * Is used for all the text within activity logs & notifications.
+ */
+return [
+
+    // Pages
+    'page_create'                 => '문서 만들기',
+    'page_create_notification'    => '페이지를 생성했습니다',
+    'page_update'                 => '문서 수정',
+    'page_update_notification'    => '페이지를 수정했습니다',
+    'page_delete'                 => '삭제 된 페이지',
+    'page_delete_notification'    => '페이지를 삭제했습니다',
+    'page_restore'                => '문서 복원',
+    'page_restore_notification'   => '페이지가 복원되었습니다',
+    'page_move'                   => '문서 이동',
+    'page_move_notification'      => 'Page successfully moved',
+
+    // Chapters
+    'chapter_create'              => '챕터 만들기',
+    'chapter_create_notification' => '챕터 생성함',
+    'chapter_update'              => '챕터 수정',
+    'chapter_update_notification' => '챕터 수정함',
+    'chapter_delete'              => '삭제된 챕터',
+    'chapter_delete_notification' => '챕터 삭제함',
+    'chapter_move'                => '챕터 이동된',
+    'chapter_move_notification' => 'Chapter successfully moved',
+
+    // Books
+    'book_create'                 => '책자 만들기',
+    'book_create_notification'    => '책 생성함',
+    'book_create_from_chapter'              => '챕터를 책으로 변환',
+    'book_create_from_chapter_notification' => '챕터를 책으로 변환했습니다.',
+    'book_update'                 => '책 수정',
+    'book_update_notification'    => '책 수정함',
+    'book_delete'                 => '책 지우기',
+    'book_delete_notification'    => '책 삭제함',
+    'book_sort'                   => '책자 정렬',
+    'book_sort_notification'      => '책 정렬 바꿈',
+
+    // Bookshelves
+    'bookshelf_create'            => '책꽃이 만들기',
+    'bookshelf_create_notification'    => '책꽃이 생성 완료',
+    'bookshelf_create_from_book'    => '책을 책꽃이로 변환',
+    'bookshelf_create_from_book_notification'    => '책을 책꽂이로 변환했습니다.',
+    'bookshelf_update'                 => '책꽃이 업데이트',
+    'bookshelf_update_notification'    => '책꽃이가 업데이트되었습니다',
+    'bookshelf_delete'                 => '선반 삭제',
+    'bookshelf_delete_notification'    => '책꽃이가 삭제되었습니다.',
+
+    // Revisions
+    'revision_restore' => '버전 복원',
+    'revision_delete' => '버전 삭제',
+    'revision_delete_notification' => '버전 삭제 성공',
+
+    // Favourites
+    'favourite_add_notification' => '":name" 북마크에 추가함',
+    'favourite_remove_notification' => '":name" 북마크에서 삭제함',
+
+    // Watching
+    'watch_update_level_notification' => 'Watch preferences successfully updated',
+
+    // Auth
+    'auth_login' => 'logged in',
+    'auth_register' => '신규 사용자 등록',
+    'auth_password_reset_request' => '사용자 비밀번호 초기화 요청',
+    'auth_password_reset_update' => '사용자 비밀번호 초기화',
+    'mfa_setup_method' => 'configured MFA method',
+    'mfa_setup_method_notification' => '다중 인증 설정함',
+    'mfa_remove_method' => 'removed MFA method',
+    'mfa_remove_method_notification' => '다중 인증 해제함',
+
+    // Settings
+    'settings_update' => '설정 변경',
+    'settings_update_notification' => '설졍 변경 성공',
+    'maintenance_action_run' => 'ran maintenance action',
+
+    // Webhooks
+    'webhook_create' => '웹 훅 만들기',
+    'webhook_create_notification' => '웹 훅 생성함',
+    'webhook_update' => '웹 훅 수정하기',
+    'webhook_update_notification' => '웹 훅 수정함',
+    'webhook_delete' => '웹 훅 지우기',
+    'webhook_delete_notification' => '웹 훅 삭제함',
+
+    // Users
+    'user_create' => '사용자 생성',
+    'user_create_notification' => '사용자 생성 성공',
+    'user_update' => '사용자 갱신',
+    'user_update_notification' => '사용자가 업데이트되었습니다',
+    'user_delete' => '사용자 삭제',
+    'user_delete_notification' => '사용자가 삭제되었습니다',
+
+    // API Tokens
+    'api_token_create' => 'aPI 토큰 생성',
+    'api_token_create_notification' => 'API token successfully created',
+    'api_token_update' => 'updated api token',
+    'api_token_update_notification' => 'API token successfully updated',
+    'api_token_delete' => 'deleted api token',
+    'api_token_delete_notification' => 'API token successfully deleted',
+
+    // Roles
+    'role_create' => '역활 생성',
+    'role_create_notification' => '역할이 생성되었습니다',
+    'role_update' => '역활 갱신',
+    'role_update_notification' => '역할이 수정되었습니다',
+    'role_delete' => '역활 삭제',
+    'role_delete_notification' => '역할이 삭제되었습니다',
+
+    // Recycle Bin
+    'recycle_bin_empty' => 'emptied recycle bin',
+    'recycle_bin_restore' => 'restored from recycle bin',
+    'recycle_bin_destroy' => 'removed from recycle bin',
+
+    // Comments
+    'commented_on'                => '댓글 쓰기',
+    'comment_create'              => '댓글 생성',
+    'comment_update'              => '댓글 변경',
+    'comment_delete'              => '댓글 삭제',
+
+    // Other
+    'permissions_update'          => '권한 수정함',
+];
